28120d30c64862f7777993c2f923cc196dd4ba89
idoubi <me@idoubi.cc>
2023-12-16 21:44:27 +0800

    show user info

--- web/app/(dashboard)/dashboard/layout.tsx
+++ web/app/(dashboard)/dashboard/layout.tsx
@@ -1,10 +1,40 @@
-import Sidebar from "./_components/Sidebar";
+"use client";
 
-export default async ({ children }: { children: React.ReactNode }) => {
+import { useEffect, useState } from "react";
+
+import Sidebar from "./_components/Sidebar";
+import { User } from "@/app/types/user";
+
+export default ({ children }: { children: React.ReactNode }) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(false);
+
+  const fetchUser = async () => {
+    setLoading(true);
+    const uri = "/api/dashboard/user-info";
+    const resp = await fetch(uri, {
+      method: "GET",
+    });
+    setLoading(false);
+
+    if (resp.ok) {
+      const res = await resp.json();
+      if (res.data) {
+        console.log("login user:", res.data);
+        setUser(res.data);
+        return;
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
       <div>
-        <Sidebar />
+        <Sidebar user={user} />
 
         <main className="py-10 lg:pl-72">
           <div className="px-4 sm:px-6 lg:px-8">{children}</div>
